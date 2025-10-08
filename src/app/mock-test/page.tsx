d42144e2db6d8e1d64fb2092cf9ac3afeaa2552d
Peter Clemenko <aoi@protonmail.ch>
2025-10-07 22:13:52 -0400

    added storybook

--- src/app/mock-test/page.tsx
+++ src/app/mock-test/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import { NftProvider } from "@/components/NftProvider";
 import { NftList } from "@/components/NftList";
 
 export default function MockTestPage() {
   return (
     <NftProvider>
-      <div className="p-6">
+      <div className="p-8">
         <h1 className="text-2xl font-bold mb-4">NFT Mock Test</h1>
         <NftList />
       </div>
